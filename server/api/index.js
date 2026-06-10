f4649bba778231d3c59cbebfd214ce534b887bb4
Grey-Hat-Guy <greyhat2703@gmail.com>
2026-06-10 22:17:52 +0530

    Configured backend for Vercel

---
+++ server/api/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("../config/db");
+const studentRoutes = require("../routes/studentRoutes");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/students", studentRoutes);
+
+module.exports = app;
--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
   try {
+    if (isConnected) return;
+
     await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
     console.log("DB Connected");
   } catch (error) {
     console.error(error.message);
